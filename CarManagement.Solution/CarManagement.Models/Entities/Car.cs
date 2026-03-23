c79c5f6f264ad25d66016343743ef71a31599e01
feixiang <913737079@qq.com>
2026-03-23 13:20:51 +1100

    feat(CM-2): create entity model

---
+++ CarManagement.Solution/CarManagement.Models/Entities/Car.cs
@@ -0,0 +1,43 @@
+﻿namespace CarManagement.Models.Entities;
+
+public class Car
+{
+    public Guid Id { get; init; }
+    public Guid DealerId { get; init; }
+    public string Make { get; init; } = null!;
+    public string Model { get; init; } = null!;
+    public int Year { get; init; }
+    public string Colour { get; set; } = null!;
+    public decimal Price { get; private set; }
+    public int StockLevel { get; private set; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset UpdatedAt { get; private set; }
+
+    public Car(
+        Guid dealerId, 
+        string make, 
+        string model, 
+        int year, 
+        string colour, 
+        decimal price, 
+        int stockLevel)
+    {
+        Id = Guid.NewGuid();
+        DealerId = dealerId;
+        Make = make;
+        Model = model;
+        Year = year;
+        Colour = colour;
+
+        if (price < 0) 
+            throw new ArgumentOutOfRangeException(nameof(price), "Price cannot be negative.");
+        Price = price;
+
+        if (stockLevel <= 0)
+            throw new ArgumentOutOfRangeException(nameof(stockLevel), "Stock level cannot be less than or equal to zero.");
+        StockLevel = stockLevel;
+
+        CreatedAt = DateTimeOffset.UtcNow;
+        UpdatedAt = CreatedAt;
+    }
+}
---
+++ CarManagement.Solution/CarManagement.Models/Entities/Dealer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CarManagement.Models.Entities;
+
+public class Dealer
+{
+    public Guid Id { get; init; }
+    public string Name { get; private set; } = null!;
+    public string Email { get; private set; } = null!;
+    public string PasswordHash { get; private set; } = null!;
+    public DateTimeOffset CreatedAt { get; init; }
+
+    public static Dealer CreateDealer(
+        string name,
+        string email,
+        string plainPassword,
+        IPasswordHasher<Dealer> passwordHasher)
+    {
+        var dealer = new Dealer
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Email = email,
+            CreatedAt = DateTimeOffset.UtcNow
+        };
+
+        dealer.PasswordHash = passwordHasher.HashPassword(dealer, plainPassword);
+
+        return dealer;
+    }
+}
